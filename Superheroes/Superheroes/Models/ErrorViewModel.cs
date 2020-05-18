f24beb8d7291ebef19ef0989bf5faa892ee37eb0
tim-morozov <tim.morozov@gmail.com>
2020-05-18 13:56:17 -0500

    uploading new project

---
+++ Superheroes/Superheroes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Superheroes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
