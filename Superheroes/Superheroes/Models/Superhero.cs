418aa5af2ed8a52dff23c985513d4f9ad1cb63ad
tim-morozov <tim.morozov@gmail.com>
2020-05-18 14:03:20 -0500

    added model for Superhero

---
+++ Superheroes/Superheroes/Models/Superhero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superheroes.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string CatchPhrase { get; set; }
+    }
+}
